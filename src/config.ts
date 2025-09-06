d5ad30ad13ce484ab44e0c70f43f508b9773370d
Inki Hong <ihong3589@gmail.com>
2025-09-06 02:07:53 +0000

    renaming container in docker-compose.yml file, and made some changes to the styling and config

--- src/config.ts
+++ src/config.ts
@@ -11,29 +11,29 @@ export const siteConfig = {
   aboutMe:
     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem quos asperiores nihil consequatur tempore cupiditate architecto natus commodi corrupti quas quasi facere est, dignissimos odit nam veniam sapiente ut, vitae eligendi ipsum dolor, nostrum ullam impedit! Corrupti ratione mollitia temporibus necessitatibus, consectetur reiciendis recusandae id, dolorum quaerat, vero pariatur. Ratione!",
   skills: ["Java", "Python", "Django", "Flask", "JavaScript", "TypeScript", "SQL", "Apex", "SpringBoot", "React", "Node.js", "Angular", "GraphQL", "Salesforce", "Kubernetes", "GitLab CI/CD", "Docker", "AWS", "Postman", "Splunk", "Oracle", "MySQL", "PostgreSQL"],
-  projects: [
-    {
-      name: "AI Dev Roundup Newsletter",
-      description:
-        "One concise email. Five minutes. Every Tuesday. Essential AI news & trends, production-ready libraries, powerful AI tools, and real-world code examples",
-      link: "https://aidevroundup.com/?ref=devportfolio",
-      skills: ["React", "Node.js", "AWS"],
-    },
-    {
-      name: "Chrome Extension Mastery: Build Full-Stack Extensions with React & Node.js",
-      description:
-        "Master the art of building production-ready, full-stack Chrome Extensions using modern web technologies and best practices",
-      link: "https://fullstackextensions.com/?ref=devportfolio",
-      skills: ["React", "Node.js", "AWS"],
-    },
-    {
-      name: "ExtensionKit",
-      description:
-        "Kit to jump-start your Chrome extension projects with a variety of battle-tested starter templates & examples",
-      link: "https://extensionkit.io/?ref=devportfolio",
-      skills: ["React", "Node.js", "AWS"],
-    },
-  ],
+  // projects: [
+  //   {
+  //     name: "AI Dev Roundup Newsletter",
+  //     description:
+  //       "One concise email. Five minutes. Every Tuesday. Essential AI news & trends, production-ready libraries, powerful AI tools, and real-world code examples",
+  //     link: "https://aidevroundup.com/?ref=devportfolio",
+  //     skills: ["React", "Node.js", "AWS"],
+  //   },
+  //   {
+  //     name: "Chrome Extension Mastery: Build Full-Stack Extensions with React & Node.js",
+  //     description:
+  //       "Master the art of building production-ready, full-stack Chrome Extensions using modern web technologies and best practices",
+  //     link: "https://fullstackextensions.com/?ref=devportfolio",
+  //     skills: ["React", "Node.js", "AWS"],
+  //   },
+  //   {
+  //     name: "ExtensionKit",
+  //     description:
+  //       "Kit to jump-start your Chrome extension projects with a variety of battle-tested starter templates & examples",
+  //     link: "https://extensionkit.io/?ref=devportfolio",
+  //     skills: ["React", "Node.js", "AWS"],
+  //   },
+  // ],
   experience: [
     {
       company: "T-Mobile | Atlanta, GA",
